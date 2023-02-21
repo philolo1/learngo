2923b96bb4287c61e9a238b4543c6d6760d0c1a8
Patrick Klitzke <invoices@kawazu-sakura-software.co.jp>
2023-02-21 23:37:58 +0900

    go server

---
+++ go_server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+	"encoding/json"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+  if r.URL.Path != "/hello" {
+    http.Error(w, "404 not found", http.StatusNotFound)
+    return
+  }
+
+  if r.Method != "GET" {
+    http.Error(w, "method not foun", http.StatusNotFound)
+    return
+  }
+	resp := make(map[string]string)
+	resp["message"] = "hello"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/form" {
+    http.Error(w, "404 not found", http.StatusNotFound)
+  }
+
+  if r.Method != "POST" {
+    http.Error(w, "method not found", http.StatusNotFound)
+  }
+
+  name := r.FormValue("name")
+  address := r.FormValue("address")
+
+  fmt.Fprintf(w, "name: %s, address %s", name, address)
+
+}
+
+func main() {
+  fileServer := http.FileServer(http.Dir("./static"))
+  http.Handle("/", fileServer)
+  http.HandleFunc("/hello", helloHandler)
+  http.HandleFunc("/form", formHandler)
+
+  fmt.Printf(" Starting server at port 8080\n")
+  if err:= http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal(err)
+  }
+}
